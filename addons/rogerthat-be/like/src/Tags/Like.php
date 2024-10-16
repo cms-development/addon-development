281ff9d6b888c2f307b7622657956dd6d4a5da8a
Frederick Roegiers <frederick@rogerthat.be>
2024-10-16 15:26:48 +0200

    wip

--- addons/rogerthat-be/like/src/Tags/Like.php
+++ addons/rogerthat-be/like/src/Tags/Like.php
@@ -9,6 +9,12 @@ class Like extends Tags {
      * The {{ like }} tag.
      */
     public function index() {
+
+        // what is the entry id ????
+        $entry_id = $this->context->get('id')->value();
+x        dd($entry_id);
+
+
         return view('like::tags.likeBtn', [
             'entry_id' => 12345
         ]);
