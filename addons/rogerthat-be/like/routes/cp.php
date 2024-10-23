a44749e7be228e2819ecea2fe9be026204a38978
Frederick Roegiers <frederick@rogerthat.be>
2024-10-23 14:23:57 +0200

    wip

--- addons/rogerthat-be/like/routes/cp.php
+++ addons/rogerthat-be/like/routes/cp.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use RogerthatBe\Like\Http\LikeCPController;
 
 Route::get('/like/index', [LikeCPController::class, "index"])->name('like.index');
-Route::get('/like/settings', [LikeCPController::class, "index"])->name('like.settings');
+Route::get('/like/settings', [LikeCPController::class, "settings"])->name('like.settings');
